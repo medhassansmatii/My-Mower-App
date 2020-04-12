ba2ffe0e9d55d24a05d64fa1a32d41cfcae82466
mohamed hassan smati <medhassansmati@gmail.com>
2020-04-12 13:09:15 +0100

    fourth commit adding log file

--- front/src/app/command.service.ts
+++ front/src/app/command.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommandService {
 
-  constructor() { }
+  private http: HttpClient;
+
+
+  constructor(http: HttpClient) { 
+    this.http=http;
+  }
+ 
+		
+
+		url="http://127.0.0.1:8080/log";
+
+  testmethod(){
+    return "hi again";
+  }
+  readJsonData(){
+    return this.http.get(this.url);
+  }
 }
--- front/src/app/ficher-log/ficher-log.component.ts
+++ front/src/app/ficher-log/ficher-log.component.ts
@@ -14,24 +14,48 @@ const httpOptions = {
   templateUrl: './ficher-log.component.html',
   styleUrls: ['./ficher-log.component.css']
 })
-export class FicherLogComponent implements OnInit {
+export class FicherLogComponent  {
 
  
-
-  etat=datax;
+  message: string;
+  txtlog:string="Cliquer sur Afficher le Log";
  
 
 	ngOnInit() { console.log(datax) ;}
-	constructor(private http: HttpClient) {}
+	constructor(private http: HttpClient) {
 
 	
+
+		
+	
+  }
+  deleteLog(){
+		let url="http://127.0.0.1:8080/deletelog";
+		this.http.delete(url).subscribe(
+ 			() =>this.txtlog="",
+		      	(err) => {console.log(err); console.log("c");}
+		    );	
+	
+	}
+
 	getLog(){
+		
+		const httpOptions2 = {
+		  headers: new HttpHeaders({
+		    'Accept': 'text/plain,*/*',
+		    'Access-Control-Allow-Headers': 'Content-Type',
+		    'Content-Type': 'text/plain',
+		  })
+		};
 		let url:string;
 		url="http://127.0.0.1:8080/log";
-		this.http.get<any>(url).subscribe(
-		      (res) => {console.log(res); },
-		      (err) => {console.log(err);}
+		this.http.get(url,{ responseType: 'text' }).subscribe(
+		      (res) => {console.log(res); this.txtlog=res;},
+		      (err) => {console.log(err); this.txtlog="le fichier est vide ou introuvable"}
 		    );	
 	}
+
+	
+	
 
 }
